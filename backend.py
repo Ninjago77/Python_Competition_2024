1d17ab26c4d27175b43d1ad3b3e258785bad1ae0
Ninjago77 <shanvantharunmozhi@gmail.com>
2024-03-10 14:39:18 -0700

    increased affine speed

--- backend.py
+++ backend.py
@@ -1,5 +1,5 @@
 from string import ascii_lowercase,ascii_uppercase # built-in library
-
+ascii_letters = ascii_lowercase+ascii_uppercase
 
 def backwardify(text:str) -> str:
     return text[::-1]
@@ -8,7 +8,7 @@ def Reverse_Cipher(text:str) -> str:
     return backwardify(text)
 
 Atbash_Mapping_Table = str.maketrans(
-    ascii_lowercase+ascii_uppercase,
+    ascii_letters,
     backwardify(ascii_lowercase)+backwardify(ascii_uppercase)
 )
 def Atbash_Cipher(text:str) -> str:
@@ -16,8 +16,7 @@ def Atbash_Cipher(text:str) -> str:
 
 Caesar_Mapping_Tables = [
     str.maketrans(
-        ascii_lowercase+
-        ascii_uppercase,
+        ascii_letters,
         ascii_lowercase[i:]+ascii_lowercase[:i]+
         ascii_uppercase[i:]+ascii_uppercase[:i])
     for i in range(26)]
@@ -37,25 +36,21 @@ def Vigenere_Cipher(text:str,key:str,to_decrypt:bool=False,decryption_key:bool=F
     return "".join([char.translate(Caesar_Mapping_Tables[ascii_uppercase_indexes[k]])
         for char,k in zip(text,key)]),(rev if decryption_key else None)
 
-Multiplier_Options = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25] # 26 coprimes
+Affine_Multiplier_Options = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25] # 26 coprimes
 def Affine_Cipher(text:str,multiplier:int,shifts:int,decrypt:bool=False):
-    if multiplier not in Multiplier_Options:
-        raise ValueError(f"Nah bro, must be in {", ".join([str(n) for n in Multiplier_Options])}")
+    if multiplier not in Affine_Multiplier_Options:
+        raise ValueError(f"Nah bro, must be in {", ".join([str(n) for n in Affine_Multiplier_Options])}")
     if decrypt: inverse_multiplier = next(i for i in range(1, 26) if (multiplier * i) % 26 == 1)
     func = (lambda index:
         ((inverse_multiplier)*(index-shifts))%26
     ) if decrypt else (lambda index:
         ((multiplier*index)+shifts)%26
     )
-    return "".join([
+    available_chars = "".join(list(filter(lambda letter: letter in ascii_letters, set(text))))
+    return text.translate(str.maketrans(available_chars,"".join([   
         ascii_lowercase[func(ascii_lowercase_indexes[char])]
             if char in ascii_lowercase else 
         ascii_uppercase[func(ascii_uppercase_indexes[char])]
             if char in ascii_uppercase else 
-        char for char in text]
-    )
-
-# # # print(Vigenere_Cipher("ndkjieidMONNWKNWKEy 372187{}~';/;./;'/","azyxwv",False,True))
-# # print(Vigenere_Cipher("nemmmjieORRSWLPZOJy 372187{}~';/;./;'/","AZYXWV",False,True))
-# print(Affine_Cipher("hdsjaiaodfDDEE",11,4,False))
-# print(Affine_Cipher("dluzeoeclhLLWW",11,4,True))
+        char for char in available_chars
+    ])))
